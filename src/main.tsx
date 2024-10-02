8fe1c4e04cc1f97f48994ca2f3bc844d7bcc5cb2
Valeria <valeria.yanycheva@gmail.com>
2024-10-02 14:07:45 +0600

    feat: add store and usersSlice

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
 import 'normalize.css'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from './store/index.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename="/">
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/store/slices/usersSlice.ts
@@ -0,0 +1,69 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IUser } from "../../types/IUser";
+
+
+export const fetchUsers = createAsyncThunk(
+  'users/fetchUsers',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+      if(!response.ok) {
+        throw new Error('Server Error!')
+      }
+
+      const data = await response.json();
+      return data;
+
+    } catch(error) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+      return rejectWithValue(new Error('Error'))
+    }
+  }
+)
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    users: [] as IUser[],
+    archiveUsers: [] as IUser[],
+    status: '',
+    error: '',
+  },
+  reducers: {
+    activeUser(state, action: PayloadAction<number>) {
+      const archivedUser = state.archiveUsers.find(user => user.id === action.payload)
+      archivedUser && state.users.push(archivedUser)
+      state.archiveUsers = state.archiveUsers.filter(user => user.id !== action.payload)
+    },
+    archiveUser(state, action: PayloadAction<number>) {
+      const activeUser = state.users.find(user => user.id === action.payload)
+      activeUser && state.archiveUsers.push(activeUser)
+      state.users = state.users.filter(user => user.id !== action.payload)
+    },
+    removeUser(state, action: PayloadAction<number>) {
+      state.users = state.users.filter((user) => user.id !== action.payload);
+      state.archiveUsers = state.archiveUsers.filter((user) => user.id !== action.payload);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.status = 'loading';
+      state.error = '';
+    })
+    .addCase(fetchUsers.fulfilled, (state, action) => {
+      state.status = 'resolved';
+      state.users = action.payload;
+    })
+    .addCase(fetchUsers.rejected, (state, action) => {
+      state.status = 'rejected';
+      action.error.message ? state.error = action.error.message : state.error = '';
+    })
+  },
+});
+
+export const {removeUser, activeUser, archiveUser} = usersSlice.actions;
+
+export default usersSlice.reducer;
